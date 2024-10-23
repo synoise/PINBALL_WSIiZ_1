cdcaac6e1bccdcb15143b72848893daf22acb4b0
WSIiZ <psynos@wsiz.edu.pl>
2024-10-23 16:51:33 +0200

    Resolved exercise - animation 2d

---
+++ Assets/Anim/WizJohny.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ClearSky;
+
+namespace ClearSky
+{
+    public class WizJohny : MonoBehaviour
+    {
+
+        public Animator anim;
+        // Start is called before the first frame update
+        public void Awake()
+        {
+            anim = GetComponent<Animator>();
+            ResetAnimation();
+            anim.SetBool("isIdle", true);
+        }
+
+        public void ResetAnimation()
+        {
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isRun", false);
+            anim.SetBool("isJump", false);
+        }
+        public  void IdleJohny()
+        {
+            ResetAnimation();
+            anim.SetTrigger("isIdle");
+        }
+        public  void RunJohny()
+        {
+            ResetAnimation();
+            anim.SetTrigger("isRun");
+        }
+        public void JumpJohny()
+        {
+            ResetAnimation();
+            anim.SetTrigger("isJump");
+        }
+    }
+}
